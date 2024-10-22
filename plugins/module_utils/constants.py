fc26df41cc3ca425b4d398053858b911e1bbc7cb
Dakota Crowder <dcrowder@redhat.com>
2024-10-22 11:01:22 -0400

    EDM-569: Add license block to constants file

--- plugins/module_utils/constants.py
+++ plugins/module_utils/constants.py
@@ -1,2 +1,6 @@
+# coding: utf-8 -*-
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 ENROLLMENT_KIND = "EnrollmentRequest"
 CSR_KIND = "CertificateSigningRequest"
