92cc03e0dba9febee8a5007da564dd69d8678053
Dakota Crowder <dcrowder@redhat.com>
2025-04-30 11:42:49 -0400

    EDM-1399: Use consts for channels and put file marker, add some test cases / docs

--- tests/unit/conftest.py
+++
@@ -1,5 +0,0 @@
-def pytest_configure(config):
-    config.option.asyncio_mode = "auto"
-    # Setting asyncio_default_fixture_loop_scope prevents many
-    # warning logs in the test runner
-    config._inicache["asyncio_default_fixture_loop_scope"] = "function"
