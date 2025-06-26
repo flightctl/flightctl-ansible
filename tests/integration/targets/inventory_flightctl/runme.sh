488b5d9fcd92f504ead0d30d8c8cd00b4fabaa1f
amalykhi <amalykhi@redhat.com>
2025-06-26 15:16:29 +0200

    EDM-1486: test ansible-doc for inventory plugin

---
+++ tests/integration/targets/inventory_flightctl/runme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+CMD_ARGS=("$@")
+
+echo "Running ansible-playbook with args: ${CMD_ARGS[*]}"
+
+
+
+# Parse flightctl_host from the integration_config.yml file and pass it to the paybook
+FLIGHTCTL_HOST=$(grep -oP 'flightctl_host:\s*\K.*' "../../integration_config.yml")
+FLIGHTCTL_TOKEN=$(grep -oP 'flightctl_token:\s*\K.*' "../../integration_config.yml")
+
+# Pass integration configuration directly; avoids fragile parsing and leaking secrets
+ansible-playbook "./inventory_doc_test.yml" -e "flightctl_host=${FLIGHTCTL_HOST}" -e "flightctl_token=${FLIGHTCTL_TOKEN}" "${CMD_ARGS[@]}" -vvv
+ansible-playbook "./inventory_test.yml" -i "./.config/flightctl/flightctl.inventory.yaml" -e "flightctl_host=${FLIGHTCTL_HOST}" -e "flightctl_token=${FLIGHTCTL_TOKEN}" "${CMD_ARGS[@]}" -vvv
+echo "DONE"
