1e084a5f16f5dde169504bda5ddd2eca9eb5c305
amalykhi <amalykhi@redhat.com>
2025-09-16 11:10:23 +0200

    fix

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -8,7 +8,7 @@ def set_module_args(args):
     # Set arguments in the format expected by Ansible modules
     # Use the original working format from older versions
     basic._ANSIBLE_ARGS = to_bytes(json.dumps({'ANSIBLE_MODULE_ARGS': args}))
-    
+
     # For compatibility with different Ansible versions, try multiple approaches
     # Don't set a profile initially - let Ansible auto-detect
     if hasattr(basic, '_ANSIBLE_PROFILE'):
