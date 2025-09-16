e395c0b265902b435060641d37dc77ceab940cf6
amalykhi <amalykhi@redhat.com>
2025-09-16 11:26:43 +0200

    fix

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -5,11 +5,8 @@ from ansible.module_utils.common.text.converters import to_bytes
 
 def set_module_args(args):
     """prepare arguments so that they will be picked up during module creation"""
-    # Set arguments in the format expected by Ansible modules
-    # Use the original working format from older versions
+    # Set arguments in the expected format
     basic._ANSIBLE_ARGS = to_bytes(json.dumps({'ANSIBLE_MODULE_ARGS': args}))
 
-    # For compatibility with different Ansible versions, try multiple approaches
-    # Don't set a profile initially - let Ansible auto-detect
-    if hasattr(basic, '_ANSIBLE_PROFILE'):
-        basic._ANSIBLE_PROFILE = None
+    # Try the most basic profile name that should exist
+    basic._ANSIBLE_PROFILE = 'basic'
