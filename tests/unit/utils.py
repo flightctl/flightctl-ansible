a7add2fb721e797ed6f3526ff6c15b09fc6c5018
amalykhi <amalykhi@redhat.com>
2025-09-16 10:59:06 +0200

    fix

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -7,11 +7,10 @@ def set_module_args(args):
     """prepare arguments so that they will be picked up during module creation"""
     # Clear any existing state first
     basic._ANSIBLE_ARGS = None
-    basic._ANSIBLE_PROFILE = None
 
     # Set up the module arguments in the format expected by current Ansible
     args_json = json.dumps({'ANSIBLE_MODULE_ARGS': args})
     basic._ANSIBLE_ARGS = to_bytes(args_json)
 
-    # Set a basic profile that should be available in most Ansible versions
-    basic._ANSIBLE_PROFILE = 'base'
+    # Set the profile that was working before - this was added to fix the error originally
+    basic._ANSIBLE_PROFILE = 'Ansible'
